726d5ec749d7cd0bdabe8cb24d80ffaedecbe6d6
DiegoOLopez <92211663+DiegoOLopez@users.noreply.github.com>
2025-11-23 01:31:51 -0600

    Pila de mensajes

--- backend/routes/chat.router.js
+++ backend/routes/chat.router.js
@@ -6,13 +6,29 @@ const router = express.Router();
 
 const service = new Chat();
 
+
+const message_list = new Set();
+
 router.post('/', async (req, res, next) => {
     try {
+        const mensajeUsuario = { 
+            author: 'user', // Valor por defecto si no envían type
+            message: message 
+        };
+        
+        message_list.add(mensajeUsuario);
         const { message, type, reset } = req.body || {};
         if (!message) {
             throw boom.badRequest('El campo "message" es requerido');
         }
         const respuesta = await service.chat(message, type, reset);
+
+        const mensajeModelo = {
+            author: 'model',
+            message: respuesta
+        }
+        message_list.add(mensajeModelo)
+        console.log(message_list)
         res.status(201).json(respuesta);
     } catch (error) {
         next(error);
